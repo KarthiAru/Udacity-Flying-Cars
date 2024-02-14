84b90df8608592d368af4892df2344c3274acde6
Karthik Arumugham <karthi.aru@gmail.com>
2024-02-14 18:24:44 +0530

    Added files for project 2 - 3D Motion Planning

---
+++ FCND-Motion-Planning/run_motion_planning.sh
@@ -0,0 +1,19 @@
+# Grid
+
+# python motion_planning.py --mode 0 --allow_diagonal 0 --prune 0 --target -122.4021033 37.79290028 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 0 --prune 1 --target -122.4021033 37.79290028 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 1 --prune 0 --target -122.4021033 37.79290028 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 1 --prune 1 --target -122.4021033 37.79290028 -0.147
+
+# python motion_planning.py --mode 0 --allow_diagonal 0 --prune 0 --target -122.39314514 37.79251198 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 0 --prune 1 --target -122.39314514 37.79251198 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 1 --prune 0 --target -122.39314514 37.79251198 -0.147
+# python motion_planning.py --mode 0 --allow_diagonal 1 --prune 1 --target -122.39314514 37.79251198 -0.147
+
+# Graph
+
+# python motion_planning.py --mode 1 --prune 0 --target -122.4021033 37.79290028 -0.147
+# python motion_planning.py --mode 1 --prune 1 --target -122.4021033 37.79290028 -0.147
+
+# python motion_planning.py --mode 1 --prune 0 --target -122.39314514 37.79251198 -0.147
+# python motion_planning.py --mode 1 --prune 1 --target -122.39314514 37.79251198 -0.147
